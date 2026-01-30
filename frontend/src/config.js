886c0dc871a135dcf3a98c8719bcf6fcfbe46308
yogesh swami <swamiyogesh670@gmail.com>
2026-01-30 21:18:34 +0530

    modify

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-// export const BACKEND_URL = "https://your-backend-name.onrender.com"; 
-export const BACKEND_URL = "http://localhost:5000";
-// export const SOCKET_URL = "wss://your-backend-name.onrender.com"; 
-export const SOCKET_URL = "ws://localhost:5000";
+export const BACKEND_URL = "https://auction-backend-2u5r.onrender.com"; 
+// export const BACKEND_URL = "http://localhost:5000";
+export const SOCKET_URL = "wss://your-backend-name.onrender.com"; 
+// export const SOCKET_URL = "ws://localhost:5000";
